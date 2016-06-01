c46ccef1de5b3291b787ee0f1bf3da2c8c2283b2
Greg Yotz <greg@yotzee.com>
2016-05-31 21:29:41 -0500

    added some bytecodes

---
+++ ByteCode.h
@@ -0,0 +1,19 @@
+//
+// Created by Greg Yotz on 5/31/16.
+//
+
+#ifndef VIRTGL_BYTECODE_H
+#define VIRTGL_BYTECODE_H
+
+static enum ByteCode{
+    HALT = 0x100, // system codes
+    ICONST,
+    IADD,ISUB,IMUL,IDIV, //integer math codes
+    FADD,FSUB,FMUL,FDIV  //float math codes
+
+} byteCode;
+
+
+
+
+#endif //VIRTGL_BYTECODE_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,57 @@
 #include <iostream>
+#include "ByteCode.h"
+
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+int fp = 0;
+int ip = 0;
+int sp = 0;
+int *stack;
+int *codeMemory;
+int *dataMemory;
+//int codeMemory
+//int dataMemory = [];
+
+void init(int dataSize, int codeSize, int stackSize){
+    fp = 0;
+    ip = 0;
+    sp = 0;
+    stack       = new int[  stackSize   ];
+    codeMemory  = new int[  codeSize    ];
+    dataMemory  = new int[  dataSize    ];
+
+}
+
+bool CPU() {
+    cout << "test" << endl;
+    int opcode = codeMemory[fp];
+    switch (opcode) {
+        case ByteCode::IADD : {
+                int x = stack[sp];
+            break;
+        }
+        case ByteCode::HALT : {
+            return false;
+            break;
+        }
+    }
+}
+
+int main(int argc, const char* argv[]) {
+    bool _running = true;
+    init(0,0,100);
+    while (_running) {
+
+        //CPU cycle
+        _running = CPU();
+#ifdef debug
+        // if in debug mode then output trace
+        outputStackTrace();
+#endif
+
+    }
+
+
     return 0;
 }
