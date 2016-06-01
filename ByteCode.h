44e63e29ad3a3983c58763d75af48ca43f8f9f8a
Greg Yotz <greg@yotzee.com>
2016-05-31 23:05:15 -0500

    added some instructions

--- ByteCode.h
+++ ByteCode.h
@@ -5,15 +5,44 @@
 #ifndef VIRTGL_BYTECODE_H
 #define VIRTGL_BYTECODE_H
 
-static enum ByteCode{
-    HALT = 0x10, // system codes
-    ICONST,PRINT,
-    IADD,ISUB,IMUL,IDIV, //integer math codes
-    FADD,FSUB,FMUL,FDIV  //float math codes
+//static enum ByteCode{
+//    HALT = 0x10, // system codes
+//    ICONST,PRINT,
+//    IADD,ISUB,IMUL,IDIV, //integer math codes
+//    FADD,FSUB,FMUL,FDIV  //float math codes
+//
+//} byteCode;
 
-} byteCode;
+static class ByteCode {
+public:
+    // System
+
+    static const int POP = 0x01;
+    static const int PUSH = 0x02;
+    static const int ICONST = 0x09;
+    static const int HALT = 0x10;
+    static const int IPRINT = 0x11;
+
+    //set value at addr into data memory
+    static const int GSTORE = 0x12;
+    //load value at addr into stack
+    static const int GLOAD = 0x13;
 
 
+    // Maths
+    static const int IADD = 0x21;
+    static const int ISUB = 0x22;
+    static const int IMUL = 0x23;
+    static const int IDIV = 0x24;
+    static const int FADD = 0x25;
+    static const int FSUB = 0x26;
+    static const int FMUL = 0x27;
+    static const int FDIV = 0x28;
+
+
+
+
+};
 
 
 #endif //VIRTGL_BYTECODE_H
--- main.cpp
+++ main.cpp
@@ -5,39 +5,95 @@
 using namespace std;
 
 int fp = 0; //frame pointer
-int cp = 0; //code pointer
+int ip = 0; //instruction pointer
 int sp = 0; //stack pointer
 int *stack = NULL;
 int *codeMemory = NULL;
 int *dataMemory = NULL;
 bool _running = true;
 
+void iprint(int msg) {
+    std::printf("SYSOUT: %d\r\n", msg);
+}
+
 void print(const char *msg) {
-    std::printf("%s\r\n",msg);
+    std::printf("SYSOUT: %s\r\n", msg);
 }
 
 void CPU() {
-    std::printf("CP:%04d\t",cp);
-    int opcode = codeMemory[cp++];
+
+    int opcode = codeMemory[ip];
+    std::printf("CP:%04d\t Inst:%d\r\n", ip, opcode);
+
+    ip++;
+
     switch (opcode) {
         case ByteCode::IADD : {
+            int b = stack[sp--];
+            int a = stack[sp--];
+            stack[++sp] = a + b;
             break;
         }
-        case ByteCode::PRINT : {
-            print("printing some stuff");
+        case ByteCode::ISUB : {
+            int b = stack[sp--];
+            int a = stack[sp--];
+            stack[++sp] = a - b;
+            break;
+        }
+        case ByteCode::IPRINT : {
+            int i = stack[sp--];
+            iprint(i);
+            break;
+        }
+        case ByteCode::ICONST : {
+            int i = codeMemory[ip++];
+            stack[++sp] = i;
+            break;
+        }
+        case ByteCode::GLOAD: {
+            int addr = codeMemory[ip++];
+            int v = dataMemory[addr];
+            stack[++sp] = v;
+            break;
+        }
+        case ByteCode::GSTORE : {
+            int v = stack[sp--];
+            int addr = codeMemory[ip++];
+            dataMemory[addr] = v;
             break;
         }
         case ByteCode::HALT : {
             _running = false;
             break;
         }
+        case 0: {
+            _running = false;
+            // code memory overflow?
+            std::printf("%s", "code memory overflow?");
+        }
     }
 }
 
+const int instructions[] = {
+
+        ByteCode::ICONST, 455,
+        ByteCode::GSTORE, 1,
+        ByteCode::GLOAD, 1,
+        ByteCode::ICONST, 45,
+        ByteCode::IADD,
+        ByteCode::IPRINT,
+        ByteCode::HALT,
+};
+
 void loadInstructionSet() {
-    codeMemory[0] = ByteCode::PRINT;
-    //codeMemory[1] = ByteCode::PRINT;
-    codeMemory[1] = ByteCode::HALT;
+    //TODO: load from file at some point
+
+    int i = 0;
+    for (i = 0; i < sizeof(instructions); i++) {
+        codeMemory[i] = instructions[i];
+    }
+
+    cout << "nInstructions: " << i << endl;
 
 }
 
@@ -50,10 +106,10 @@ int main(int argc, const char *argv[]) {
     int startPoint = 0;
 
     fp = 0;
-    cp = startPoint;
+    ip = startPoint;
     sp = -1;
-    stack = new int[stackSize];
 
+    stack = new int[stackSize];
     codeMemory = new int[codeSize];
     dataMemory = new int[dataSize];
 
@@ -63,6 +119,7 @@ int main(int argc, const char *argv[]) {
         //CPU cycle
         CPU();
     }
+
     delete[] stack;
     delete[] codeMemory;
     delete[] dataMemory;
